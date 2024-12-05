53dd06a78e49db69f4c5178d39e1ec2f801d6976
Élcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2024-12-05 06:46:06 -0300

    Remove Navbar component from Helper, Jobs, and Queue pages

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/Navbar";
 function Home() {
   return (
     <div>
-      <Navbar />
         <h1>Olá, você está na página de jobs da aplicação!</h1>
     </div>
   );
--- src/app/queue/page.tsx
+++ src/app/queue/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/Navbar";
 function Home() {
   return (
     <div>
-      <Navbar />
         <h1>Olá, você está na página de queue da aplicação!</h1>
     </div>
   );
